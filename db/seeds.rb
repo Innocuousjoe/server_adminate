9a170f297adb443c562831d9b9c03fbe4e3850a6
Nick Coelius <ncoelius@gmail.com>
2020-09-01 10:18:00 -0700

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,133 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+
+Task.create(name: "Craft, And Your Instrument", description: "Change up your rhythm, whether in your acting life or in your non-acting
+life, by playing with your words and your thoughts as if you're conducting a symphony, expressing nuances via crescendo or a change in pacing.", points: 1)
+Task.create(name: "Craft, And Your Instrument", description: "Break down a character: objectives, intention, backstory, status.", points: 1)
+Task.create(name: "Craft, And Your Instrument", description: "Break down a scene: structure, subtext, dialogue, story arc.", points: 1)
+Task.create(name: "Craft, And Your Instrument", description: "Audit a craft class.", points: 1)
+Task.create(name: "Craft, And Your Instrument", description: "Go see a play.", points: 1)
+Task.create(name: "Craft, And Your Instrument", description: "Attend an open mic night.", points: 1)
+Task.create(name: "Craft, And Your Instrument", description: "Go to — or even participate in — an improv jam.", points: 1)
+Task.create(name: "Craft, And Your Instrument", description: "Sing karaoke.", points: 1)
+Task.create(name: "Craft, And Your Instrument", description: "Attend the table read of a work-in-progress (participate if you can).", points: 1)
+
+Task.create(name: "Research And Prep", description: "Read a play you've never read before.", points: 1)
+Task.create(name: "Research And Prep", description: "Read a chapter in a biography or autobiography of an actor 
+	whose career and craft you admire", points: 1)
+Task.create(name: "Research And Prep", description: "Read a script written by someone whose work you hope to voice someday.", points: 1)
+Task.create(name: "Research And Prep", description: "Read three pages in a trade publication — every word — and mark 
+	names of players about whom you want to know more someday.", points: 1)
+Task.create(name: "Research And Prep", description: "Hit the local library and read a chapter in a book you're not sure you
+	want to buy; people-watch while you're there.", points: 1)
+Task.create(name: "Research And Prep", description: "Watch a great movie or episode of a TV series with the director
+	commentary turned on. Take notes!", points: 1)
+Task.create(name: "Research And Prep", description: "Explore a new podcast about the entertainment industry and listen
+	to a few archived episodes.", points: 1)
+Task.create(name: "Research And Prep", description: "Update your show bible using Google Alerts that have been flowing
+	into your inbox.", points: 1)
+Task.create(name: "Research And Prep", description: "Update your show bible using notes from recent networking activities.", points: 1)
+Task.create(name: "Research And Prep", description: "Explore a new podcast about the entertainment industry and listen
+	to a few archived episodes.", points: 1)
+Task.create(name: "Research And Prep", description: "Spend an hour tracking the career moves of your pace car; that
+	person who is just one tier ahead of you — look at what roles they were taking on a few years ago and where they were planting seeds
+	to get where they are now.", points: 1)
+Task.create(name: "Research And Prep", description: "Do a lap at Actors Access and Casting Networks to be sure you've self
+	submitted on projects that line up with your bullseye.", points: 1)
+Task.create(name: "Research And Prep", description: "Check your Actors Access and Casting Networks messages for any
+	requests for self-taped auditions or visits to the session room.", points: 1)
+Task.create(name: "Research And Prep", description: "Study your targets: spend time watching the resources that your 
+	targets are putting out there via blogs, vlogs, printed interviews, and their own social networking profiles", points: 1)
+Task.create(name: "Research And Prep", description: "Watch an archived livestream from the SAG-AFTRA Foundation
+	library (even if you're not yet a SAG-AFTRA member).", points: 1)
+Task.create(name: "Research And Prep", description: "Watch an episode of a TV show from which you previously
+	downloaded sides to track how the finished product looks (which lines are thrown away, what type of actor 
+	booked the role, any relationships you can track on IMDb-Pro) and consider choices you can make in future 
+	reads that may make you feel like a booker.", points: 1)
+Task.create(name: "Research And Prep", description: "Watch an episode of each new series that makes it to TV to track
+	trends and keep tabs on what your showrunner targets are up to; also check in on hot webseries and series that go 
+	straight to Netflix, Hulu, Amazon, etc., and update your show bible accordingly.", points: 1)
+
+Task.create(name: "Mindset", description: "Build your brain in ways that Facebook cannot by doing a half-hour of
+	Lumosity, Words With Friends, Sudoku, or a pen-and-paper logic puzzle.", points: 1)
+Task.create(name: "Mindset", description: "Face your fears by writing down three things that terrify you about
+	pursuing a creative career (save conquering those fears for another day; for now, just dissect 'em a bit).", points: 1)
+Task.create(name: "Mindset", description: "Do something 'little' differently, y'know, like brushing your teeth with
+	your non-dominant hand or driving a different route than you always do between two points.", points: 1)
+Task.create(name: "Mindset", description: "Do some non-acting enrichment using a site like Skillshare, Coursera,
+	Udemy, or Creative Live; then watch a TED Talk on something you imagine might be boring.", points: 1)
+Task.create(name: "Mindset", description: "Mentor a newbie — and if you have no access to young, up-and-coming
+	actors who could benefit from your expertise, contribute to the collective knowledge base that exists at 
+	free message boards online, where simple words of encouragement or reminders that actors are never to pay 
+	up-front for representation can be hugely helpful.", points: 1)
+Task.create(name: "Mindset", description: "Set goals for the week, the month, the quarter, the year, 5 years, 10 years —
+	leave yourself lots of dreaming room here, and don't get so rigidly locked into anything that this task becomes torture.", points: 1)
+Task.create(name: "Mindset", description: "Write a list of those to whom you will always be grateful, as you
+	continue to succeed in your creative career; especially note those who will be thanked in those 'holding up something gold and shiny'
+	moments to come.", points: 1)
+Task.create(name: "Mindset", description: "Journal, write freely, do the 'morning pages' à la The Artist\'s Way by
+	Julia Cameron.", points: 1)
+
+Task.create(name: "Tools Of The Trade", description: "Test various pieces of your auditioning wardrobe on camera using
+	your self-taping set-up; discover what each color and neckline and pattern does to your skintone and overall vibe.", points: 1)
+Task.create(name: "Tools Of The Trade", description: "Test your self-taping set-up at different times of day to see what the
+	existence of natural light at various hours does to your artificial lighting.", points: 1)
+Task.create(name: "Tools Of The Trade", description: "Style your hair and makeup different ways as you self-tape to track
+	the impact of a little shine-reducing powder.", points: 1)
+Task.create(name: "Tools Of The Trade", description: "Make sure your receipts are organized and categorized so it's not a
+	bear of a task, come tax time.", points: 1)
+Task.create(name: "Tools Of The Trade", description: "Revisit your recent paystubs to be sure your agency commissions are
+	being properly calculated and reported.", points: 1)
+Task.create(name: "Tools Of The Trade", description: "Accounting analysis: tally up your union dues, photos, acting classes,
+	membership on submission sites, and marketing materials.", points: 1)
+Task.create(name: "Tools Of The Trade", description: "Contact a producer from whose project you've not yet received your
+	footage; be polite and professional, yet firm in asking for an expected date by which you will receive your scenes for 
+	your reel, even if the team is not yet finished with final edits on the project.", points: 1)
+Task.create(name: "Tools Of The Trade", description: "Perform some resume feng shui.", points: 1)
+Task.create(name: "Tools Of The Trade", description: "Update your bio.", points: 1)
+Task.create(name: "Tools Of The Trade", description: "Revamp your cover letter.", points: 1)
+Task.create(name: "Tools Of The Trade", description: "Tighten up your reel.", points: 1)
+Task.create(name: "Tools Of The Trade", description: "Update your social networking presence — spruce up that 'about me'
+	logline, refresh your photos, check your Googleability.", points: 1)
+Task.create(name: "Tools Of The Trade", description: "Reach out to your fans — shoot an email, write a new blog post, put
+	together some postcards for an upcoming targeted mailing.", points: 1)
+Task.create(name: "Tools Of The Trade", description: "Do a targeted mailing of postcards, general submissions, or career
+	updates — again, a targeted mailing.", points: 1)
+Task.create(name: "Tools Of The Trade", description: "Update your website.", points: 1)
+
+Task.create(name: "Mastering The Game", description: "Watch a half-hour of junket footage from a recent movie's press tour
+	using the 'related videos' feature on YouTube; learn which interviewers employ what style, observe which actors handle 
+	curveballs best, note when answers are crafted by publicists or improvised by the actors — think about what your style will be.", points: 1)
+Task.create(name: "Mastering The Game", description: "Visit WireImage or Getty Images' editorial-entertainment galleries and
+	search out the fashion choices of actors in your type and vibe, to see what they wear to red carpet events, charity appearances, 
+	upfronts, luncheons, or any other time they're photographed (and not just by TMZ).", points: 1)
+Task.create(name: "Mastering The Game", description: "Search out discount versions of key items you'd like to add to your
+	networking wardrobe — get smart with the best colors for you and add one great piece to your closet (while purging 
+	the items that simply don't work for on-brand you).", points: 1)
+Task.create(name: "Mastering The Game", description: "Drill yourself on your monologues — yes, even if you are never asked to
+	perform them; don't let 'em get rusty.", points: 1)
+Task.create(name: "Mastering The Game", description: "Drill yourself on your 16 bars — yes, even if you don't really audition for
+	musical theatre any more; you may need 'em someday.", points: 1)
+Task.create(name: "Mastering The Game", description: "Drill yourself on your pitch, your logline, your answer to, 'So, tell me
+	about yourself.'", points: 1)
+Task.create(name: "Mastering The Game", description: "Write a scene for yourself and two friends.", points: 1)
+Task.create(name: "Mastering The Game", description: "Shoot a scene — do it for your reel, do it for the practice, do it for
+	relationship-building, just do it.", points: 1)
+Task.create(name: "Mastering The Game", description: "Transcribe and practice. That means if you're targeting a
+	show written by Aaron Sorkin, you transcribe a lesser-known Sorkin scene and practice its pacing, its rhythm, 
+	its style (also avoid iconic characters).", points: 1)
+Task.create(name: "Mastering The Game", description: "Watch actors on everything from Inside the Actors Studio to Watch
+	What Happens Live to study the art of Brandprov.", points: 1)
+Task.create(name: "Mastering The Game", description: "Watch a 'dumb reality show' that you otherwise would never care
+	about, and pay particular attention to the music, and how producers are branding good guys and bad guys with their 
+	choices in sound editing — master producer-end branding by deconstructing it and considering what your theme music might be.", points: 1)
+Task.create(name: "Mastering The Game", description: "Hit up a local film festival, panel discussion, theatre company fundraiser,
+	or student film showcase for some low-stakes networking experiences.", points: 1)
+Task.create(name: "Mastering The Game", description: "Engage in a DIY power group — find other creatives who might
+	like to jam about what's happening in the industry, your community, your personal and professional lives and then do so, regularly; 
+	split up targets and share the research load, thereby creating a beautiful mastermind experience.", points: 1)
+Task.create(name: "Mastering The Game", description: "Do something from your 'things I love to do for my acting career' list
+	that's not otherwise represented by these prompts.", points: 1)
+Task.create(name: "Mastering The Game", description: "Do something from your 'things I hate to do for my acting career' list
+	that's not otherwise represented by these prompts.", points: 1)
+
+
